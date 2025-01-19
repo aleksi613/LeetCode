6d77d4213c5e059b36906aee60722752002a275b
Aleksi Valioghli <145508040+aleksi613@users.noreply.github.com>
2025-01-19 02:27:03 -0500

    Time: 1 ms (99.77%), Space: 45.7 MB (93.66%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,29 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
 
-        if(nums.length <= 1){
-            return;
-        }
-
-        Deque<Integer> dequeNums = new ArrayDeque<>();
-
-        Deque<Integer> dequeZeros = new ArrayDeque<>();
+        int index = 0;
 
         for (int i = 0; i < nums.length; i++){
             if (nums[i] != 0){
-                dequeNums.addLast(nums[i]);
-            } else{
-                dequeZeros.addFirst(nums[i]);
+                nums[index++] = nums[i];
             }
         }
-        
-        dequeNums.addAll(dequeZeros);
 
-        Integer[] tempArray = dequeNums.toArray(new Integer[0]);
-
-        for(int k = 0; k < tempArray.length; k++){
-            nums[k] = tempArray[k];
+        while (index < nums.length){
+            nums[index++] = 0;
         }
-
     }
 }
