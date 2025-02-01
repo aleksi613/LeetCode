f041711012ebf7c7817de5e85f4682355cc1f4f6
Aleksi Valioghli <145508040+aleksi613@users.noreply.github.com>
2025-02-01 03:17:08 -0500

    Time: 1 ms (37.58%), Space: 43 MB (97%) - LeetHub

---
+++ 3429-special-array-i/3429-special-array-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+
+        boolean parity = true;
+
+        if (nums.length == 1){
+            return parity;
+        }
+
+    for(int i = 0; i < nums.length - 1; i++){
+
+        if(nums[i] % 2 == 0 && nums[i + 1] % 2 == 0 || nums[i] % 2 != 0 && nums[i + 1] % 2 != 0){
+            return parity = false;
+        }
+    }
+
+    return parity;
+    }
+}
