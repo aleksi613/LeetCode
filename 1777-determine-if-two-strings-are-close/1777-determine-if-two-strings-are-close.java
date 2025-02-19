a667e3f7d2a2c0d1d467254699ddc06028ce6991
Aleksi Valioghli <145508040+aleksi613@users.noreply.github.com>
2025-02-18 20:18:13 -0500

    Time: 12 ms (57.47%), Space: 46 MB (15.09%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
@@ -1,40 +1,27 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
+        
+        if (word1.length() != word2.length()) return false;
 
-        HashMap<Character, Integer> map1 = new HashMap<>();
-        HashMap<Character, Integer> map2 = new HashMap<>();
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        boolean[] exists1 = new boolean[26];
+        boolean[] exists2 = new boolean[26];
 
-        if(word1.length() != word2.length()){
-            return false;
+        for (char c : word1.toCharArray()) {
+            freq1[c - 'a']++;
+            exists1[c - 'a'] = true;
+        }
+        for (char c : word2.toCharArray()) {
+            freq2[c - 'a']++;
+            exists2[c - 'a'] = true;
         }
 
-        char[] charArray1 = word1.toCharArray();
-        char[] charArray2 = word2.toCharArray();
+        if (!Arrays.equals(exists1, exists2)) return false;
 
-        for(char c : charArray1){
-            map1.put(c, map1.getOrDefault(c, 0) + 1); 
-        }
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
 
-        for(char c : charArray2){
-            map2.put(c, map2.getOrDefault(c, 0) + 1); 
-        }
-
-        if(!map1.keySet().equals(map2.keySet())){
-            return false;
-        }
-
-        // check if freq distribution is the same
-        List<Integer> list1 = new ArrayList<>(map1.values());
-        List<Integer> list2 = new ArrayList<>(map2.values());
-
-        Collections.sort(list1);
-        Collections.sort(list2);
-
-        if(list1.equals(list2)){
-            return true;
-        } else{
-            return false;
-        }
-
+        return Arrays.equals(freq1, freq2);
     }
 }
